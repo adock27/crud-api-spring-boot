5697110fad52df6ba52dd3c00a51e3d230fb54ed
adock27 <andersondumet@gmail.com>
2023-04-10 21:48:16 -0500

    lombook added and refactonred models

--- src/main/java/com/example/demo/models/ArticuloModel.java
+++ src/main/java/com/example/demo/models/ArticuloModel.java
@@ -11,7 +11,12 @@ import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
+@Getter @Setter @NoArgsConstructor
 @Table(name = "articulos")
 public class ArticuloModel {
     @Id
@@ -35,111 +40,5 @@ public class ArticuloModel {
     @JsonIgnoreProperties("articulos")
     @ManyToOne(optional = false)
     private EstudianteModel usuario_id;
-
-   
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getMarca() {
-        return marca;
-    }
-
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
-
-    public String getPrecioventa() {
-        return precioventa;
-    }
-
-    public void setPrecioventa(String precioventa) {
-        this.precioventa = precioventa;
-    }
-
-    public String getPreciocompra() {
-        return preciocompra;
-    }
-
-    public void setPreciocompra(String preciocompra) {
-        this.preciocompra = preciocompra;
-    }
-
-    public String getIva() {
-        return iva;
-    }
-
-    public void setIva(String iva) {
-        this.iva = iva;
-    }
-
-    public String getModelo() {
-        return modelo;
-    }
-
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
-    }
-
-    public String getProveedor() {
-        return proveedor;
-    }
-
-    public void setProveedor(String proveedor) {
-        this.proveedor = proveedor;
-    }
-
-    public String getTienda() {
-        return tienda;
-    }
-
-    public void setTienda(String tienda) {
-        this.tienda = tienda;
-    }
-
-    public String getCantidad() {
-        return cantidad;
-    }
-
-    public void setCantidad(String cantidad) {
-        this.cantidad = cantidad;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public String getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(String categoria) {
-        this.categoria = categoria;
-    }
-
-    public String getFecha() {
-        return fecha;
-    }
-
-    public void setFecha(String fecha) {
-        this.fecha = fecha;
-    }
 
 }
--- src/main/java/com/example/demo/models/EstudianteModel.java
+++ src/main/java/com/example/demo/models/EstudianteModel.java
@@ -8,13 +8,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
+@Getter @Setter @NoArgsConstructor
 @Table(name = "Estudiante")
 public class EstudianteModel {
     @Id
@@ -34,99 +38,5 @@ public class EstudianteModel {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "id")
     private List<ArticuloModel> articulos;
 
-    // public String getUniversidad() {
-    // return universidad;
-    // }
-    // public void setUniversidad(String universidad) {
-    // this.universidad = universidad;
-    // }
-
-    public List<ArticuloModel> getArticulos() {
-        return articulos;
-    }
-
-    public void setArticulos(List<ArticuloModel> articulos) {
-        this.articulos = articulos;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getFechaNacimiento() {
-        return fechaNacimiento;
-    }
-
-    public void setFechaNacimiento(String fechaNacimiento) {
-        this.fechaNacimiento = fechaNacimiento;
-    }
-
-    public String getSemestre() {
-        return semestre;
-    }
-
-    public void setSemestre(String semestre) {
-        this.semestre = semestre;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getGenero() {
-        return genero;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-
-    public String getTeléfono() {
-        return teléfono;
-    }
-
-    public void setTeléfono(String teléfono) {
-        this.teléfono = teléfono;
-    }
-
-    public String getPrograma() {
-        return programa;
-    }
-
-    public void setPrograma(String programa) {
-        this.programa = programa;
-    }
-
-    public String getUniversidad() {
-        return universidad;
-    }
-
-    public void setUniversidad(String universidad) {
-        this.universidad = universidad;
-    }
-
+  
 }
